e91aee9545d595f049ff788f3b9952c288577112
ikanago <28985004+ikanago@users.noreply.github.com>
2020-05-15 17:51:48 +0900

    ✨ Read code from command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+#[macro_use]
+extern crate clap;
+
 use ojisan_fuck::interpreter::Interpreter;
 
 fn main() {
-    let code = "++++++[>++++++++<-]++++++++++[>.+<-]";
-    let mut interpreter = Interpreter::transpile_from(code);
-    interpreter.eval();
+    let matches = clap_app!(ojisan_fuck =>
+        (version: crate_version!())
+        (author: crate_authors!())
+        (about: crate_description!())
+        (@arg CODE: +required "Input source code.")
+        (@arg transpile: --transpile "If specified, transpile from basic brainfuck code.")
+    )
+    .get_matches();
+
+    if let Some(ref code) = matches.value_of("CODE") {
+        let mut interpreter = if matches.is_present("transpile") {
+            Interpreter::transpile_from(code)
+        } else {
+            Interpreter::new(code)
+        };
+        interpreter.eval();
+    } else {
+        eprintln!("Specify source code.");
+        panic!();
+    }
 }
